45b25ee4ff37e2c8a86155d2a451b61c201d4f1d
nathenqian <zczyqdc@126.com>
2016-05-05 15:43:14 +0800

    add product search function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -31,6 +31,13 @@ class ProductsController < ApplicationController
   end
 
   def search
-    
+    @limitl = params["limitl"]
+    @limitr = params["limitr"]
+    @search_content = params["content"]
+    @products = Product.find_by_sql("select * from products where 
+      tag like '%#{search_content}%' or 
+      title like '%#{search_content}%' or 
+      description like '%#{search_content}%'
+      order by id limit #{limitl}, #{limitr}")
   end
 end
