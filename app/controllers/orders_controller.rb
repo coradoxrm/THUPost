a17cb8ebf8476adbc5b0ea3dc19dd86ce17f7c1b
Paul Zhu <zhufengminpaul@163.com>
2016-05-12 17:07:23 +0800

    add order ajax

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,13 @@
 class OrdersController < ApplicationController
+  # NOTE: fix CSRF in future
+  skip_before_filter :verify_authenticity_token
+
   def create
     @product = Product.find(params[:id])
     @order = current_user.orders.build(
-      product: @product,
-      price: params[:price],
-      description: params[:description]
+        product: @product,
+        price: params[:price],
+        description: params[:description]
     )
     @order.save
     redirect_to product_path(@product)
@@ -13,5 +16,16 @@ class OrdersController < ApplicationController
   def index
     @orders = current_user.orders
   end
-  
+
+  def notify_email
+    @order = Product.find(params[:order_id])
+    UserMailer.notify_email(@order).deliver_now
+    @res = {:code => 0}
+    render :json => @res
+  end
+
+  def notify_text
+    @res = {:code => 0}
+    render :json => @res
+  end
 end
