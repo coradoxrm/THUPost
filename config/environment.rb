975a902e8aacfccabb937f8f8354ab0c5c267a76
renmiao <coradoxrm@163.com>
2016-05-29 13:34:06 +0800

    fix my order ui

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ Paperclip.options[:image_magick_path] = "/usr/local/Cellar/imagemagick/6.9.3-7/b
 Paperclip.options[:command_path] = "/usr/local/Cellar/imagemagick/6.9.3-7/bin/"
 # Initialize the Rails application.
 Rails.application.initialize!
-config.time_zone = 'Beijing'
-config.active_record.default_timezone = :local
+#config.time_zone = 'Beijing'
+#config.active_record.default_timezone = :local
