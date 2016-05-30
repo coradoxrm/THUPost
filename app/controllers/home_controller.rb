e17c3f3452e7f2111d0061a670b9164f008b6a59
renmiao <coradoxrm@163.com>
2016-05-31 00:07:36 +0800

    delete dependency and collection show page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,32 @@ class HomeController < ApplicationController
   end
 
   def index
-    @products = get_category("电子产品", 0, 40)
+    @products = Product.find_by_sql("select * from products order by id limit #{0}, #{20}")
+
+    my_products = current_user.products
+    @order_count = 0
+    @likes_count = 0
+    for p in my_products
+      if (p.status != 2)
+        @order_count += p.orders.length
+      end
+
+      
+
+    end
+
+    my_orders = current_user.orders
+    @notif_count = 0
+    for o in my_orders
+      if(o.status == 1)
+        @notif_count += 1
+      end
+    end
+
+
+
+
+
   end
 
   def minor
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   has_many :orders, :dependent => :delete_all
+  has_many :collections, :dependent => :delete_all
   #{x: 4, y: 0}
   #{:x => 4, :y => 0}
 
