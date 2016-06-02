d62a7ae2d579d82c828e0d90bd687f5b0dea9d8b
renmiao <coradoxrm@163.com>
2016-06-03 01:03:25 +0800

    show status and custom devise alert

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -5,17 +5,53 @@ module DeviseHelper
   #
   # This method is intended to stay simple and it is unlikely that we are going to change
   # it to add more behavior or options.
-  def devise_error_messages!
-    return '' if resource.errors.empty?
 
-    messages = resource.errors.full_messages.join '<br/>'
+  def devise_error_messages!
+    flash_alerts = []
+    error_key = 'errors.messages.not_saved'
+
+    if !flash.empty?
+      flash_alerts.push(flash[:error]) if flash[:error]
+      flash_alerts.push(flash[:alert]) if flash[:alert]
+      flash_alerts.push(flash[:notice]) if flash[:notice]
+      error_key = 'devise.failure.invalid'
+    end
+
+    return "" if resource.errors.empty? && flash_alerts.empty?
+    errors = resource.errors.empty? ? flash_alerts : resource.errors.full_messages
+
+    messages = errors.map { |msg| content_tag(:span, msg) }.join
+    # sentence = I18n.t(error_key, :count    => errors.count,
+    #                              :resource => resource.class.model_name.human.downcase)
 
     html = <<-HTML
-    <div id="error_explanation">
-      <p class="alert">#{messages}</p>
-    </div>
+
+    <div class="alert alert-danger alert-dismissable">
+                    <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+                    #{messages}
+                </div>
     HTML
 
     html.html_safe
   end
+
+
+  # def devise_error_messages!
+  #   return '' if resource.errors.empty?
+  #
+  #   messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+  #   sentence = I18n.t('errors.messages.not_saved',
+  #     count: resource.errors.count,
+  #     resource: resource.class.model_name.human.downcase)
+  #
+  #   html = <<-HTML
+  #   <div class="alert alert-error alert-block">
+  #     <button type="button" class="close" data-dismiss="alert">x</button>
+  #     <h4>#{sentence}</h4>
+  #     #{messages}
+  #   </div>
+  #   HTML
+  #
+  #   html.html_safe
+  # end
 end
