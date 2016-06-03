e399139997441bccafff87b959948936074d3e39
renmiao <coradoxrm@163.com>
2016-06-03 20:36:24 +0800

    delete with notification

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
-         
+
   has_many :products, :dependent => :delete_all
   has_many :orders, :dependent => :delete_all
 
-  has_attached_file :avatar, styles: { small: "64x64", med: "100x100", large: "200x200" }, default_url: "/images/empty.jpg"
+  has_attached_file :avatar, styles: { small: "100x100#" }, default_url: "/images/empty.jpg"
   # validates_attachment :avatar, presence: true,
   validates_attachment_content_type :avatar, content_type: [/\Aimage\/.*\Z/, nil, ""]
   # validates_attachment_size :avatar, in: 0..100000.kilobytes
