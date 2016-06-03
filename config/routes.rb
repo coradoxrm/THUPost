e156e413b17698a6b7461e0ff84831ecfcb37e4e
renmiao <coradoxrm@163.com>
2016-06-03 21:58:50 +0800

    registration

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  devise_for :users #, controllers: {registrations: "registrations",confirmations: 'confirmations'}
+  devise_for :users , controllers: {registrations: "registrations"}
   resources :users
 
   get 'home/index'
