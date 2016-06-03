2c6bff078198c91b8e04b8523512a02b2d8fd0ea
Paul Zhu <zhufengminpaul@163.com>
2016-06-03 14:59:23 +0800

    update mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,19 +1,7 @@
 class UserMailer < ApplicationMailer
-  def welcome_email(user)
-    @user = user
-    @url = 'http://azure.paulz.site:3000'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-  end
-
-  def test_my_mailer(user)
-    @user = user
-    print('test ok')
-    print(@user)
-  end
-
   def notify_email(order)
     @order = order
     @user = @order.user
-    mail(to: @user.email, subject: '通知')
+    mail(to: @user.email, subject: '卖家确认通知')
   end
 end
