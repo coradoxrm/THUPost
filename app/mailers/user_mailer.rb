5609f5906e09f7e105008c84bbddedc8dec7687a
Paul Zhu <zhufengminpaul@163.com>
2016-06-22 23:27:50 +0800

    add new emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,16 @@ class UserMailer < ApplicationMailer
     @user = @order.user
     mail(to: @user.email, subject: '卖家确认通知')
   end
+
+  def notify_seller_email(order)
+    @order = order
+    @seller = @order.product.user
+    mail(to: @seller.email, subject: '交易确认通知')
+  end
+
+  def notify_order_email(order)
+    @order = order
+    @seller = @order.product.user
+    mail(to: @seller.email, subject: '订单创建通知')
+  end
 end
