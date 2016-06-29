16372344841be4abc35636331662af34ced09d1e
renmiao <coradoxrm@163.com>
2016-06-29 21:56:42 +0800

    product img unfixed

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,30 +6,30 @@ class Product < ActiveRecord::Base
   #{:x => 4, :y => 0}
 
   #has_attached_file :photo0, styles: { small: "64x64", med: "100x100", large: "200x200", thumb: "400x400#" },
-  has_attached_file :photo0, styles: { thumb: "400x400" },
+  has_attached_file :photo0, styles: { thumb: "400x400#" },
     default_url: "/images/empty.jpg"
 
   validates_attachment_content_type :photo0, content_type: [/\Aimage\/.*\Z/, nil, ""]
 
   #has_attached_file :photo1, styles: { small: "64x64", med: "100x100", large: "200x200", thumb: "400x400#" },
-  has_attached_file :photo1, styles: { thumb: "400x400" },
+  has_attached_file :photo1, styles: { thumb: "400x400#" },
     default_url: "/images/empty.jpg"
 
   validates_attachment_content_type :photo1, content_type: [/\Aimage\/.*\Z/, nil, ""]
 
   #has_attached_file :photo2, styles: { small: "64x64", med: "100x100", large: "200x200", thumb: "400x400#" },
-  has_attached_file :photo2, styles: { thumb: "400x400" },
+  has_attached_file :photo2, styles: { thumb: "400x400#" },
     default_url: "/images/empty.jpg"
 
   validates_attachment_content_type :photo2, content_type: [/\Aimage\/.*\Z/, nil, ""]
 
   #has_attached_file :photo3, styles: { small: "64x64", med: "100x100", large: "200x200", thumb: "400x400#" },
-  has_attached_file :photo3, styles: { thumb: "400x400" },
+  has_attached_file :photo3, styles: { thumb: "400x400#" },
     default_url: "/images/empty.jpg"
   validates_attachment_content_type :photo3, content_type: [/\Aimage\/.*\Z/, nil, ""]
 
   #has_attached_file :photo4, styles: { small: "64x64", med: "100x100", large: "200x200", thumb: "400x400#" },
-  has_attached_file :photo4, styles: { thumb: "400x400" },
+  has_attached_file :photo4, styles: { thumb: "400x400#" },
     default_url: "/images/empty.jpg"
   validates_attachment_content_type :photo4, content_type: [/\Aimage\/.*\Z/, nil, ""]
 
